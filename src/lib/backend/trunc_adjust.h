8e369fa9b51731bcfdca1a2e5ce496b0bfa7d689
Seunghyeon Nam <seunghyeon.nam@sf.snu.ac.kr>
2024-05-17 18:01:44 +0900

    Add pass to enforce correct trunc behavior (#87)

---
+++ src/lib/backend/trunc_adjust.h
@@ -0,0 +1,13 @@
+#ifndef SC_BACKEND_TRUNC_ADJUST_H
+#define SC_BACKEND_TRUNC_ADJUST_H
+
+#include "llvm/IR/PassManager.h"
+
+namespace sc::backend::trunc_adjust {
+class TruncateAdjustPass
+    : public llvm::PassInfoMixin<TruncateAdjustPass> {
+public:
+  llvm::PreservedAnalyses run(llvm::Module &, llvm::ModuleAnalysisManager &);
+};
+} // namespace sc::backend::trunc_adjust
+#endif // SC_BACKEND_TRUNC_ADJUST_H
