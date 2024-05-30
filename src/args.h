377377ca2507cdfc5aa4eb26787cebeb6a84929a
strikef <seunghyeon.nam@sf.snu.ac.kr>
2024-05-30 18:58:00 +0900

    Bump version

--- src/args.h
+++ src/args.h
@@ -4,7 +4,7 @@
  * @file args.h
  * @author SWPP TAs (swpp@sf.snu.ac.kr)
  * @brief swpp-compiler argument parser module
- * @version 2024.1.11
+ * @version 2024.1.12
  * @date 2024-05-30
  * @copyright Copyright (c) 2022-2024 SWPP TAs
  */
--- src/lib/print_ir.h
+++ src/lib/print_ir.h
@@ -5,7 +5,7 @@
  * @file print_ir.h
  * @author SWPP TAs (swpp@sf.snu.ac.kr)
  * @brief Module to conditionally print the given LLVM IR program
- * @version 2024.1.11
+ * @version 2024.1.12
  * @date 2024-05-30
  * @copyright Copyright (c) 2022-2024 SWPP TAs
  */
